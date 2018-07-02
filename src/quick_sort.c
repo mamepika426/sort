ad304a23e643c5f3fec4204ee7e4997ad91f5446
Yamaguchi Shota <yamaguchi.s.au@m.titech.ac.jp>
2018-07-02 22:17:24 +0900

    課題

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
 
+
 //フェルマー素数
 #define N ((1<<16)+1)
 
 int A[N];
 
 /*
-A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
-*/
+ A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
+ */
 void quick_sort(int A[], int n){
+    if (n==1){
+        return;
+    }
+    if (n==0){
+        return ;
+    }
+    
+    else {
+        pivot =A[0];
+        for(i=j=1;i<n;i++){
+            if(A[i] <= pivot){
+                int z =A[j];
+                A[j]=A[i];
+                A[i]=z;
+                j++;
+            }
+        }
+    }
+    k=A[0];
+    A[0]=A[j-1];
+    A[j-1]=k;
+    quick_sort(A,j-1);
+    quick_sort(A+j,n-j);
 }
 
 int main(){
-  int i;
-  A[0] = 0;
-  A[1] = 3; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
-  }
-
-  quick_sort(A, N);
-  for(i=0;i<N;i++){
-    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
-  }
+    int i;
+    A[0] = 0;
+    A[1] = 3; //原始元
+    for(i=2;i<N;i++){
+        A[i] = (long long int) A[i-1] * A[1] % N;
+    }
+    
+    quick_sort(A, N);
+    for(i=0;i<N;i++){
+        if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+    }
 }
--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -6,23 +6,57 @@
 int A[N];
 
 /*
-A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
-*/
+ A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
+ */
 void quick_sort(int A[], int n){
+    if (n==1){
+        return;
+    }
+    if (n==0){
+        return;
+    }
+    else{
+        int i,j,pivot,l;
+        l=n;
+        pivot = A[0];
+        for(i = j = 0;i < l; i++){
+            if (A[i] < pivot){
+                int z = A[j];
+                A[j] = A[i];
+                A[i] = z;
+                j++;
+            }
+            if (A[i] > pivot){
+                l--;
+                int z = A[l];
+                A[l] = A[i];
+                A[i] = z;
+                i--;
+            }
+        }
+        quick_sort(A,j);
+        quick_sort(A+l,n-l);
+        
+    }
 }
 
+
+
+
+
+
 int main(){
-  int i;
-  A[0] = 0;
-  A[1] = 3; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
-  }
-
-// すべての要素が同じ場合でも計算が早く終わるか確認する
-
-  quick_sort(A, N);
-  for(i=0;i<N;i++){
-    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
-  }
+    int i;
+    A[0] = 0;
+    A[1] = 3; //原始元
+    for(i=2;i<N;i++){
+        A[i] = (long long int) A[i-1] * A[1] % N;
+    }
+    
+    // すべての要素が同じ場合でも計算が早く終わるか確認する
+    
+    quick_sort(A, N);
+    for(i=0;i<N;i++){
+        if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+    }
 }
